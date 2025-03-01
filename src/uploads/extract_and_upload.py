eb1f96a5061646cf2f38e27b91bd764b03f6cdfc
Edu Bravo <eduardobravopires@gmail.com>
2025-03-01 13:46:05 -0300

    fix: format numbered list

--- src/uploads/extract_and_upload.py
+++ src/uploads/extract_and_upload.py
@@ -1,8 +1,8 @@
 import os
 
 from dotenv import load_dotenv
-from src.uploads.extract_notion_pages import extract_notion_pages
-from src.uploads.upload_pinecone import upload_pinecone
+from uploads.extract_notion_pages import extract_notion_pages
+from uploads.upload_pinecone import upload_pinecone
 
 load_dotenv()
 NOTION_DATABASE_ID = os.getenv("NOTION_DATABASE_ID")
--- src/uploads/extract_notion_pages.py
+++ src/uploads/extract_notion_pages.py
@@ -2,7 +2,6 @@ from notion_client import Client
 import os
 
 NOTION_API_KEY = os.getenv("NOTION_API_KEY")
-
 notion = Client(auth=NOTION_API_KEY)
 
 def extract_notion_pages(database_id):
@@ -11,12 +10,26 @@ def extract_notion_pages(database_id):
 
     for page in response["results"]:
         page_id = page["id"]
+        page_contents = []
         blocks = notion.blocks.children.list(block_id=page_id)
+        
+        numbered_list_counter = 1  # Contador para listas numeradas
+
         for block in blocks["results"]:
             if "type" in block and "rich_text" in block[block["type"]]:
                 rich_text_content = block[block["type"]]["rich_text"]
+                
+                text_content = "".join(text_item["text"]["content"] for text_item in rich_text_content)
 
-                for text_item in rich_text_content:
-                    extracted_texts.append(text_item["text"]["content"].replace("\n", ""))
+                # Se for um item de lista numerada, adiciona o número manualmente
+                if block["type"] == "numbered_list_item":
+                    text_content = f"{numbered_list_counter}. {text_content}"
+                    numbered_list_counter += 1  # Incrementa o contador
+                else:
+                    numbered_list_counter = 1  # Reseta o contador se não for lista numerada
+
+                page_contents.append(text_content)
+
+        extracted_texts.append(" ".join(page_contents))
 
     return extracted_texts
