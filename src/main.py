15bedf7d2118d183d5ae5183076ac19099df6cab
Edu Bravo <eduardobravopires@gmail.com>
2025-03-01 13:45:59 -0300

    fix: allow cors

--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from chatbot import start_chatbot
 from langchain_core.messages import HumanMessage, SystemMessage
+from fastapi.middleware.cors import CORSMiddleware
+
+from chatbot import start_chatbot
 
 load_dotenv()
 
 app = FastAPI()
 
 chatbot = start_chatbot()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todas as origens (para produção, use um domínio específico)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 class ChatRequest(BaseModel):
     message: str
